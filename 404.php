2b5b104ce43f1cc08ffbcd3855d9bc79e9592d12
Msxixi <156132455+Msxixi@users.noreply.github.com>
2026-04-08 14:37:14 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<section class="container section">
+  <h1>页面走丢了</h1>
+  <p>抱歉，你访问的页面不存在。</p>
+  <p><a class="btn" href="<?php echo esc_url(home_url('/')); ?>">返回首页</a></p>
+</section>
+<?php get_footer(); ?>
